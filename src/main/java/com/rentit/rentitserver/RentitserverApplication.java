4880403aa3c731d27b652e6e1badfc92e073641a
Atul <aatulporwal999@gmail.com>
2025-08-31 23:28:47 +0530

    Initial Commit

---
+++ src/main/java/com/rentit/rentitserver/RentitserverApplication.java
@@ -0,0 +1,13 @@
+package com.rentit.rentitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentitserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentitserverApplication.class, args);
+    }
+
+}
