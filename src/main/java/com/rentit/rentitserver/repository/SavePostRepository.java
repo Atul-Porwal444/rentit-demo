b5aa93f945b0b9aa4a05153bb31a7a43d0171d63
Atul <aatulporwal999@gmail.com>
2025-09-15 01:29:24 +0530

    Added the API for the getting all the post saved by the user using the user id.

--- src/main/java/com/rentit/rentitserver/repository/SavePostRepository.java
+++ src/main/java/com/rentit/rentitserver/repository/SavePostRepository.java
@@ -3,6 +3,8 @@ package com.rentit.rentitserver.repository;
 import com.rentit.rentitserver.entity.SavedPostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SavePostRepository extends JpaRepository<SavedPostEntity, Long> {
+import java.util.List;
 
+public interface SavePostRepository extends JpaRepository<SavedPostEntity, Long> {
+    List<SavedPostEntity> findBySavedBy_UserId(Long userId);
 }
